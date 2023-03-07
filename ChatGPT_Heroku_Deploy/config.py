8eae955c2ef90ee67e42f946329d4307dd24ffe0
Nehla A <namin.general@gmail.com>
2023-03-07 18:46:36 -0500

    edited Config.py to change postgres uri

--- ChatGPT_Heroku_Deploy/config.py
+++ ChatGPT_Heroku_Deploy/config.py
@@ -9,5 +9,10 @@ class Config(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.urandom(12).hex()
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = uri
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
