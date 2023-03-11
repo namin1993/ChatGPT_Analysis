b686608380125608c0753b08f877ea378a384b61
Nehla A <namin.general@gmail.com>
2023-03-11 11:42:42 -0500

    Corrected create_tables function name in commands.py

--- app/commands.py
+++ app/commands.py
@@ -4,7 +4,7 @@ from flask.cli import with_appcontext
 from app import db
 from .models import User, Message
 
-@click.command(name='create_table')
+@click.command(name='create_tables')
 @with_appcontext
 def create_tables():
     db.create_all()
