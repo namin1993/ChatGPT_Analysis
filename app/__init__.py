73e50248748758a1960083242bbca0cfab57ad11
Nehla A <namin.general@gmail.com>
2023-04-09 08:07:51 -0400

    added keyring email setup in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import yagmail
+import keyring
 import os
 
 # Initialization
@@ -16,6 +18,11 @@ migrate = Migrate(app, db)
 
 DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
 engine = create_engine(DB_URI)
+
+# Setup email
+EMAIL_ACCOUNT = app.config['EMAIL_ACCOUNT']
+EMAIL_API_KEY = app.config['EMAIL_API']
+keyring.set_password("yagmail", EMAIL_ACCOUNT, EMAIL_API_KEY)
 
 # Flask-login
 login = LoginManager(app)
